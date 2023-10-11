55affc1cb9b56fba831a389d62f4cb4af8f60303
juliawgraham <juliagra@andrew.cmu.edu>
2023-10-11 12:42:38 -0400

    experimented with locations (not entirely working)

--- 67443-sprint3/Views/ContentView.swift
+++ 67443-sprint3/Views/ContentView.swift
@@ -4,13 +4,23 @@
 //
 
 import SwiftUI
+
 import GoogleSignIn
+
+import MapKit
+import CoreLocation
 
 struct ContentView: View {
   @EnvironmentObject var authViewModel: AuthenticationViewModel
+  
+  @StateObject var manager = LocationManager()
 
   var body: some View {
     return Group {
+     
+      Map(coordinateRegion: $manager.region, showsUserLocation: true)
+                  .edgesIgnoringSafeArea(.all)
+      
       NavigationView {
         switch authViewModel.state {
         case .signedIn:
