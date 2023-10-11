eac289437de3a93ed780a206d36b702fe2abb653
juliawgraham <juliagra@andrew.cmu.edu>
2023-10-10 21:00:04 -0400

    Initial Commit

---
+++ 67443-sprint3/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  67443-sprint3
+//
+//  Created by Julia Graham on 10/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 67443-sprint3/_7443_sprint3App.swift
@@ -0,0 +1,17 @@
+//
+//  _7443_sprint3App.swift
+//  67443-sprint3
+//
+//  Created by Julia Graham on 10/10/23.
+//
+
+import SwiftUI
+
+@main
+struct _7443_sprint3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
