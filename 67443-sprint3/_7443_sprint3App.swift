5ad8388683bf9c5eafc92c3b1e779624ca4a9cfe
juliawgraham <juliagra@andrew.cmu.edu>
2023-10-23 13:47:08 -0400

    set up firebase

--- 67443-sprint3/_7443_sprint3App.swift
+++ 67443-sprint3/_7443_sprint3App.swift
@@ -6,11 +6,24 @@
 //
 
 import SwiftUI
+
 import GoogleSignIn
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+    return true
+  }
+}
 
 @main
 struct _7443_sprint3App: App {
   @StateObject var authViewModel = AuthenticationViewModel()
+  
+  // register app delegate for Firebase setup
+  @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
 
   var body: some Scene {
     WindowGroup {
