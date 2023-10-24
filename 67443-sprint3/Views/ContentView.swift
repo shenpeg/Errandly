b5036d7b4a98208161f85e0d2a235a01ea3fc627
juliawgraham <juliagra@andrew.cmu.edu>
2023-10-24 10:20:25 -0400

    removed everything except oauth and firebase from main branch

--- 67443-sprint3/Views/ContentView.swift
+++ 67443-sprint3/Views/ContentView.swift
@@ -6,39 +6,12 @@
 import SwiftUI
 
 import GoogleSignIn
-import CoreLocation
 
 struct ContentView: View {
   @EnvironmentObject var authViewModel: AuthenticationViewModel
-  @StateObject var locationPermission:LocationViewModel=LocationViewModel()
-    
-  @State var phoneNumber = ""
   
   var body: some View {
     return Group {
-      TextField("Enter phone number", text: $phoneNumber)
-      Button(
-        action: {
-          MessagesService().sendMessage(phoneNumber)
-        },
-        label: {
-          Text("Send message")
-        }).buttonStyle(.bordered)
-      
-      LocationPermissionView(locationPermission: locationPermission)
-      
-      VStack {
-        Text("Distance from current location to CMU in meters: 40.4432° N (lat), 79.9428° W (long)")
-        switch locationPermission.authorizationStatus {
-          case .authorizedAlways, .authorizedWhenInUse:
-          Text(String(CLLocation(latitude: locationPermission.cordinates?.latitude ?? 0.0, longitude: locationPermission.cordinates?.longitude ?? 0.0).distance(from: CLLocation(latitude: 40.4432, longitude: -79.9428))))
-          default:
-            Text("No current location")
-        }
-      }
-      
-      LocationSearchView(locationSearchService: LocationSearchService())
-
       NavigationView {
         switch authViewModel.state {
         case .signedIn:
