31403a8ada5125817cb5fb69d62dda69ed40b445
peggy <98036446+shenpeg@users.noreply.github.com>
2023-10-31 20:29:30 -0400

    Changed some simple visual formatting

--- 67443-sprint3/Views/Errand/ErrandDetailsView.swift
+++ 67443-sprint3/Views/Errand/ErrandDetailsView.swift
@@ -8,27 +8,59 @@
 import SwiftUI
 
 struct ErrandDetailsView: View {
-  var errand: Errand
+    var errand: Errand
 
-  var body: some View {
-    let dateFormat = DateFormatter()
-    dateFormat.dateFormat = "YY/MM/dd"
-    
-    return VStack(alignment: .leading) {
-      Text("Name: \(errand.name)")
-      Text("Description: \(errand.description)")
-      Text("Pay: $\(errand.pay)")
-      Text("Location: \(errand.location.latitude), \(errand.location.longitude)")
-      Text("Date posted: \(dateFormat.string(from: errand.date_posted))")
-      Text("Date due: \(dateFormat.string(from: errand.date_due))")
-      Text("Status: \(errand.status)")
-      Text("Owner name: \(errand.owner.first_name) \(errand.owner.last_name)")
+    var body: some View {
+        let dateFormat = DateFormatter()
+        dateFormat.dateFormat = "MM/dd/YY"
+        let payFormat = String(format: "$%.2f", errand.pay)
+
+        return ScrollView {
+            VStack(alignment: .leading, spacing: 10) {
+                Text(errand.name)
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .foregroundColor(.primary)
+              
+                Text("\(errand.owner.first_name) \(errand.owner.last_name)")
+                      .font(.headline)
+                      .foregroundColor(.primary)
+                
+                Text(errand.description)
+                    .font(.body)
+                    .foregroundColor(.secondary)
+                
+                HStack {
+                    Text("Pay:")
+                        .font(.headline)
+                        .foregroundColor(.primary)
+                    Text(payFormat)
+                        .font(.headline)
+                        .foregroundColor(.green)
+                }
+                
+                HStack {
+                    Text("Location:")
+                    Text("\(errand.location.latitude), \(errand.location.longitude)")
+                }
+                
+                HStack {
+                    Text("Date Posted:")
+                    Text(dateFormat.string(from: errand.date_posted))
+                }
+                
+                HStack {
+                    Text("Date Due:")
+                    Text(dateFormat.string(from: errand.date_due))
+                }
+                
+                HStack {
+                    Text("Status:")
+                    Text(errand.status)
+                }
+            }
+            .padding(20)
+            .navigationBarTitle("Errand Details", displayMode: .inline)
+        }
     }
-    .navigationBarTitle(Text("Errand Details"), displayMode: .inline)
-    .frame(alignment: .leading)
-  }
 }
-
-//#Preview {
-//    ErrandDetailsView()
-//}
