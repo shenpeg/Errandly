6d6827017fec365b5da569254f46633f6d79548b
peggy <98036446+shenpeg@users.noreply.github.com>
2023-11-08 16:06:43 -0500

    Formatting errand detail profile and changes from main

--- 67443-sprint3/Views/Errand/ErrandDetailsProfileView.swift
+++ 67443-sprint3/Views/Errand/ErrandDetailsProfileView.swift
@@ -10,51 +10,53 @@ import SwiftUI
 import CoreLocation
 
 struct ErrandDetailsProfileView: View {
-    var errand: Errand
-    @StateObject private var viewModel = LocationTimeFormatViewModel()
+  @ObservedObject var usersViewModel: UsersViewModel = UsersViewModel()
+  var errand: Errand
+  @StateObject private var viewModel = LocationTimeFormatViewModel()
 
 
   var body: some View {
-      let dateFormat = DateFormatter()
-      dateFormat.dateFormat = "MM/dd/YY"
-      let timeDifference = viewModel.calculateTimeDifference(from: errand.datePosted)
+    let errandOwnerUser = usersViewModel.getUser(errand.owner.id)
 
-      return HStack {
-          AsyncImage(url: URL(string: "https://via.placeholder.com/34x34")) { phase in
-              switch phase {
-              case .success(let image):
-                  image
-                      .resizable()
-                      .aspectRatio(contentMode: .fill)
-                      .frame(width: 34, height: 34)
-                      .clipShape(Circle())
-              case .empty:
-                  EmptyView()
-              case .failure:
-                  Image(systemName: "xmark.octagon")
-              @unknown default:
-                  EmptyView()
-              }
+    let dateFormat = DateFormatter()
+    dateFormat.dateFormat = "MM/dd/YY"
+    let timeDifference = viewModel.calculateTimeDifference(from: errand.datePosted)
+
+    return HStack {
+      
+      VStack{
+          UserProfileImageView(pfp: errand.owner.pfp, size: 32)
+      }
+    
+      VStack(alignment: .leading){
+          if (errandOwnerUser != nil) {
+            NavigationLink(destination:
+                            UserProfileView(user: errandOwnerUser, isCurUser: false)
+            ) {
+              Text("\(errand.owner.first_name) \(errand.owner.last_name)")
+                .font(.headline)
+            }
+            .accentColor(.black)
+          }
+          else {
+            Text("\(errand.owner.first_name) \(errand.owner.last_name)")
+              .font(.headline)
+              .foregroundColor(.primary)
           }
           
-          VStack(alignment: .leading) {
-              Text("\(errand.owner.first_name) \(errand.owner.last_name)")
-                  .font(.headline)
-                  .foregroundColor(.primary)
-              HStack {
-                  Text(viewModel.locationName)
-                      .font(.footnote)
-                      .foregroundColor(.secondary)
-                  Text("|").font(.footnote).foregroundColor(.secondary)
-                  Text(viewModel.formatTimeDifference(timeDifference))
-                      .font(.footnote)
-                      .foregroundColor(.secondary)
-              }
+          HStack{
+            Text(viewModel.locationName)
+                .font(.footnote)
+                .foregroundColor(.secondary)
+            Text("|").font(.footnote).foregroundColor(.secondary)
+            Text(viewModel.formatTimeDifference(timeDifference))
+                .font(.footnote)
+                .foregroundColor(.secondary)
           }
+        }
       }
       .onAppear {
         viewModel.getLocationName(for: CLLocation(latitude: errand.location.latitude, longitude: errand.location.longitude))
       }
   }
-
 }
