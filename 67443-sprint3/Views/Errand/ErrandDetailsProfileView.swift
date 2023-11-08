244e3c836841956f56016f3bb00af7669a89861b
peggy <98036446+shenpeg@users.noreply.github.com>
2023-11-08 02:11:48 -0500

    Moved ErrandDetailProfile functions to a view model

--- 67443-sprint3/Views/Errand/ErrandDetailsProfileView.swift
+++ 67443-sprint3/Views/Errand/ErrandDetailsProfileView.swift
@@ -11,12 +11,13 @@ import CoreLocation
 
 struct ErrandDetailsProfileView: View {
     var errand: Errand
-    @State private var locationName: String = ""
+    @StateObject private var viewModel = LocationTimeFormatViewModel()
+
 
   var body: some View {
       let dateFormat = DateFormatter()
       dateFormat.dateFormat = "MM/dd/YY"
-      let timeDifference = calculateTimeDifference(from: errand.datePosted)
+      let timeDifference = viewModel.calculateTimeDifference(from: errand.datePosted)
 
       return HStack {
           AsyncImage(url: URL(string: "https://via.placeholder.com/34x34")) { phase in
@@ -41,49 +42,19 @@ struct ErrandDetailsProfileView: View {
                   .font(.headline)
                   .foregroundColor(.primary)
               HStack {
-                  Text(locationName)
+                  Text(viewModel.locationName)
                       .font(.footnote)
                       .foregroundColor(.secondary)
                   Text("|").font(.footnote).foregroundColor(.secondary)
-                  Text(formatTimeDifference(timeDifference))
+                  Text(viewModel.formatTimeDifference(timeDifference))
                       .font(.footnote)
                       .foregroundColor(.secondary)
               }
           }
       }
       .onAppear {
-          getLocationName(for: CLLocation(latitude: errand.location.latitude, longitude: errand.location.longitude))
+        viewModel.getLocationName(for: CLLocation(latitude: errand.location.latitude, longitude: errand.location.longitude))
       }
   }
 
-    func calculateTimeDifference(from date: Date) -> TimeInterval {
-        let currentTime = Date()
-        return currentTime.timeIntervalSince(date)
-    }
-
-    func formatTimeDifference(_ timeDifference: TimeInterval) -> String {
-        let formatter = DateComponentsFormatter()
-        formatter.allowedUnits = [.second, .minute, .hour, .day, .weekOfMonth, .month, .year]
-        formatter.maximumUnitCount = 1
-        formatter.unitsStyle = .abbreviated
-
-        if let formattedString = formatter.string(from: timeDifference) {
-            return formattedString + " ago"
-        } else {
-            return "Unknown"
-        }
-    }
-    
-    // Fetch location name using reverse geocoding
-    func getLocationName(for location: CLLocation) {
-        let geocoder = CLGeocoder()
-        geocoder.reverseGeocodeLocation(location) { placemarks, error in
-            if let error = error {
-                print("Error in reverse geocoding: \(error.localizedDescription)")
-            } else if let placemark = placemarks?.first {
-                // Display state name with placemark.administrativeArea
-                self.locationName = placemark.administrativeArea ?? "Unknown State"
-            }
-        }
-    }
 }
