902b78f8d07eef4518c142639353c652aa265dff
juliawgraham <juliagra@andrew.cmu.edu>
2023-12-02 17:16:42 -0500

    Apple pay button

---
+++ 67443-sprint3/Views/Pay/PaymentButton.swift
@@ -0,0 +1,18 @@
+//
+//  PaymentButton.swift
+//  67443-sprint3
+//
+//  Created by Julia Graham on 12/2/23.
+//
+
+import SwiftUI
+
+struct PaymentButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PaymentButton()
+}
