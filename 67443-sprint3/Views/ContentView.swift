0a2d2aa5f36b1cf8b6fbf1a3093a84dabefe1129
peggy <98036446+shenpeg@users.noreply.github.com>
2023-12-02 15:48:49 -0500

    Onboarding tutorial for new users (#26)
    * working tutorial steps with clumsy UI
    
    * Updated tutorial SVG file
    
    * fixed tutorial overlay effect when using tutorial button
    
    * ensure tutorial appears for only first time open
    
    * precise padding and tutorial button changes
    
    * minor padding edits
    
    * fixing more minor padding issues

--- 67443-sprint3/Views/ContentView.swift
+++ 67443-sprint3/Views/ContentView.swift
@@ -20,6 +20,7 @@ struct ContentView: View {
   @StateObject var locationViewModel: LocationViewModel = LocationViewModel()
   @State private var marketplacePath = NavigationPath()
   @State private var profilePath = NavigationPath()
+  @State private var isOnboardingPresented = true
 
   init() {
     UITabBar.appearance().backgroundColor = .white
@@ -58,7 +59,17 @@ struct ContentView: View {
     .environmentObject(errandsViewModel)
     .environmentObject(tabUtil)
     .environmentObject(locationViewModel)
-  }
+    .onAppear {
+        if UserDefaults.standard.bool(forKey: "hasSeenTutorial") == false {
+            isOnboardingPresented = true
+            UserDefaults.standard.set(true, forKey: "hasSeenTutorial")
+        }
+    }
+    .overlay(
+        isOnboardingPresented ? TutorialView(isOnboardingPresented: $isOnboardingPresented)
+                               .transition(.opacity) : nil
+    )
+}
   
   private func tabSelection() -> Binding<Int> {
     Binding {
