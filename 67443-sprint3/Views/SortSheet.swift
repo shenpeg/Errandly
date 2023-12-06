ec2732ce0e6ac3c9b483f16042352b92e9a1b19e
peggy <98036446+shenpeg@users.noreply.github.com>
2023-12-06 01:07:46 -0500

    bold sort text of current sorting

--- 67443-sprint3/Views/SortSheet.swift
+++ 67443-sprint3/Views/SortSheet.swift
@@ -1,8 +1,6 @@
 //
 //  SortSheet.swift
-//  67443-sprint3
-//
-//  Created by Ohnyoo Esther Bae on 11/9/23.
+//  Errandly
 //
 
 import Foundation
@@ -10,39 +8,64 @@ import SwiftUI
 import UIKit
 
 struct SortSheet: View {
-  @EnvironmentObject var errandsViewModel: ErrandsViewModel
-  @Environment(\.dismiss) var dismiss
-  
-  @Binding var searchField: String
-  @Binding var selectedTags: String // : [String] = []
+    @EnvironmentObject var errandsViewModel: ErrandsViewModel
+    @Environment(\.dismiss) var dismiss
+
+    @Binding var searchField: String
+    @Binding var selectedTags: String // : [String] = []
+
+    @State private var isRecentClicked = false
+    @State private var isDueDateClicked = false
+    @State private var isCompensationClicked = false
 
     var body: some View {
-      let errands = errandsViewModel.errands
-      
-      VStack {
-        Text("Sort Errands By:")
-          .font(.title2)
-          .padding()
-        Divider()
-        Button {
-          errandsViewModel.errands = errands.sorted(by: {$0.datePosted > $1.datePosted} )
-          errandsViewModel.filterErrands(searchText: self.searchField, selectedTags: self.selectedTags)
-        } label: {
-          Text("Recent")
-        }.padding()
-        Button {
-          errandsViewModel.errands = errands.sorted(by: {$0.dateDue < $1.dateDue} )
-          errandsViewModel.filterErrands(searchText: self.searchField, selectedTags: self.selectedTags)
-        } label: {
-          Text("Due Date")
-        }.padding()
-        Button {
-          errandsViewModel.errands = errands.sorted(by: {$0.pay > $1.pay} )
-          errandsViewModel.filterErrands(searchText: self.searchField, selectedTags: self.selectedTags)
-        } label: {
-          Text("Compensation")
-        }.padding()
-      }
-      .presentationDetents([.height(300)])
+        let errands = errandsViewModel.errands
+
+        VStack {
+            Text("Sort Errands By:")
+                .font(.title2)
+                .padding()
+            Divider()
+
+            Button {
+                isRecentClicked.toggle()
+                isDueDateClicked = false
+                isCompensationClicked = false
+
+                errandsViewModel.errands = errands.sorted(by: { $0.datePosted > $1.datePosted })
+                errandsViewModel.filterErrands(searchText: self.searchField, selectedTags: self.selectedTags)
+            } label: {
+                Text("Recent")
+                    .fontWeight(isRecentClicked ? .bold : .regular)
+            }
+            .padding()
+
+            Button {
+                isRecentClicked = false
+                isDueDateClicked.toggle()
+                isCompensationClicked = false
+
+                errandsViewModel.errands = errands.sorted(by: { $0.dateDue < $1.dateDue })
+                errandsViewModel.filterErrands(searchText: self.searchField, selectedTags: self.selectedTags)
+            } label: {
+                Text("Due Date")
+                    .fontWeight(isDueDateClicked ? .bold : .regular)
+            }
+            .padding()
+
+            Button {
+                isRecentClicked = false
+                isDueDateClicked = false
+                isCompensationClicked.toggle()
+
+                errandsViewModel.errands = errands.sorted(by: { $0.pay > $1.pay })
+                errandsViewModel.filterErrands(searchText: self.searchField, selectedTags: self.selectedTags)
+            } label: {
+                Text("Compensation")
+                    .fontWeight(isCompensationClicked ? .bold : .regular)
+            }
+            .padding()
+        }
+        .presentationDetents([.height(300)])
     }
 }
